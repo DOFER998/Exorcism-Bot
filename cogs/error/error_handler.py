4b85eb2e32547cfab6260a8f4e56908f59e5466b
DOFER998 <killmy666@gmail.com>
2023-09-15 14:19:23 +0300

    Added error handler

---
+++ cogs/error/error_handler.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+
+class ErrorHandler(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_application_command_error(self, ctx, error):
+        handled = False
+
+        async def send_error(*args, **kwargs):
+            await ctx.respond(*args, ephemeral=True, **kwargs)
+
+        if (cog := ctx.cog):
+            if cog._get_overridden_method(cog.cog_command_error) is not None:
+                handled = True
+                return
+        ignore = (commands.CommandNotFound)
+        if isinstance(error, ignore):
+            handled = True
+            return
+        if isinstance(error, commands.NotOwner):
+            handled = True
+            await send_error("Извините, только владелец бота и сообщества может использовать эту команду!",
+                             delete_after=10)
+
+
+def setup(bot):
+    bot.add_cog(ErrorHandler(bot))
